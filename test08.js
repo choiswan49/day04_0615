817115ebaa8bc6b33b35956fb2f7d4148d822e58
choiswan49 <evaswan429@gmail.com>
2023-06-15 21:43:22 +0900

    write, read, unlink, etc…

--- test08.js
+++ test08.js
@@ -10,9 +10,34 @@ const fileOptions = async()=>{
         const data = await fsPromises.readFile('./datas/lorem.txt', "utf-8")
         console.log("1. readfile", data);
         console.log("2. console.log");
+
+        await fsPromises.writeFile('./datas/newWrite.txt', data);
+        console.log('3. write complete');
+
+        await fsPromises.appendFile('./datas/newWrite.txt', 'append txt');
+        console.log('4. appendFile');
+
+        await fsPromises.rename('./datas/newWrite.txt', './datas/rename.txt', err=>{if(err) throw err});
+        console.log('5. rename');
+
+        if (!fs.existsSync('./model')){
+            await fs.mkdir("./model", err=> {if(err) throw err});
+        }
+        console.log('6. mkdir')
+
+        // fs.readdir('./', (err, filelist)=>{
+        //     console.log(filelist)    
+        // })
+        const filelist = await fsPromises.readdir('./', err=>{if(err) throw err});
+        console.log('7.', filelist);
+
+        await fsPromises.unlink(path.join(__dirname, "datas", "rename.txt"));
+        console.log('8. unlink');
     }catch(err){
         // err 처리
         console.log(err);
+        // err=>{if(err) throw err}
+        // err 발생시 err 받아서 처리
     }finally{
         // 생략 가능
         // try 코드가 실행돼도, catch 구문이 실행돼도 실행
